e86c1943f4ea81eb86c92d3a1b3977ea3eab67b3
EpicVon2468 <thomaschad0909@gmail.com>
2025-11-21 10:23:44 +1100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+	id("java")
+	kotlin("jvm") version "2.2.21"
+}
+
+group = "io.github.epicvon2468.leetcodesolutions"
+version = "1.0.0"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {}
